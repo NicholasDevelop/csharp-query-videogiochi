aca895c282b1bc560d795819ee665f1533ac2eec
NicholasDevelop <marcaccini.n@gmail.com>
2022-07-08 23:12:59 +0200

    Completate le prime 10 query

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -24,15 +24,29 @@ FROM tournaments
 WHERE year = 2015;
 
 -- 6- Selezionare tutti i premi che contengono nella descrizione la parola 'facere' (2)
+SELECT *
+FROM awards
+WHERE description like '%facere%';
 
 -- 7- Selezionare tutti i videogame che hanno la categoria 2 (FPS) o 6 (RPG), mostrandoli una sola volta (del videogioco vogliamo solo l'ID) (287)
+SELECT DISTINCT videogame_id
+FROM category_videogame
+WHERE category_id = 2 OR category_id = 6;
 
 -- 8- Selezionare tutte le recensioni con voto compreso tra 2 e 4 (2947)
+SELECT *
+FROM reviews
+WHERE rating >= 2 and rating <= 4;
 
 -- 9- Selezionare tutti i dati dei videogiochi rilasciati nell'anno 2020 (46)
+SELECT *
+FROM videogames
+WHERE DATEPART(year,release_date) = 2020;
 
--- 10- Selezionare gli id dei videogame che hanno ricevuto almeno una recensione da stelle, mostrandoli una sola volta (443)
-
+-- 10- Selezionare gli id dei videogame che hanno ricevuto almeno una recensione da 5 stelle, mostrandoli una sola volta (443)
+SELECT DISTINCT videogame_id
+FROM reviews
+WHERE rating = 5;
 
 -- *********** BONUS ***********
 -- 
